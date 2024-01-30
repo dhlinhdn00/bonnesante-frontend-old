24b7c51e6db9d4b22d82fb49d346418fced1b39b
LeVHoangduc <levhoangduc@gmail.com>
2024-01-30 19:48:24 +0700

    updates

--- src/pages/Doctors/CheckScheduled/CheckScheduled.jsx
+++ src/pages/Doctors/CheckScheduled/CheckScheduled.jsx
@@ -1,12 +1,56 @@
+import { ref, child, get, update, onValue } from "firebase/database";
+import { database } from "../../../services/firebase/config";
+
 import style from './CheckScheduled.module.css'
 
 import check from "../../../assets/icons/check.svg"
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import HeaderBar from '../../../components/HeaderBar/HeaderBar'
 import { Link } from 'react-router-dom'
 
+
 export default function CheckScheduled() {
+    console.log("re render")
+    const [schedules, setSchedules] = useState([{}])
+
+
+    const dbRef = ref(database);
+
+    onValue((child(dbRef, 'videoCall/')), (snapshot) => {
+        const data = snapshot.val();
+        console.log(data)
+        const schedulesArray = Object.values(data);
+
+        if (schedulesArray.length !== schedules.length) {
+            setSchedules(schedulesArray);
+        }
+    });
+
+
+    const handleConfirm = (values) => {
+        const userConfirmed = schedules.find((value) => value.userID === values.userID)
+
+        console.log(userConfirmed)
+
+        const updates = {
+            [`videoCall/${userConfirmed.userID}/isAccepted`]: true
+        };
+
+        console.log(updates)
+        // Get a reference to the database
+        const dbRef = ref(database);
+
+        // Update the specified location with the updates object
+        return update(dbRef, updates)
+            .then(() => {
+                console.log(`User with ID ${userConfirmed.userID} has been confirmed.`);
+            })
+            .catch((error) => {
+                console.error('Error updating user confirmation:', error);
+            });
+    }
+
     return (
         <>
             <div className={style.header}>
@@ -16,11 +60,13 @@ export default function CheckScheduled() {
                 <h1 className={style.title}>Appointments are available</h1>
 
                 <ul>
-                    <li className={style.schedule}>Monday 29 January
-                        <Link to="/doctor/meeting">
-                            <img src={check} alt="check" className={style.check} />
-                        </Link>
-                    </li>
+                    {schedules?.map((value, index) => {
+                        return (<li key={index} className={style.schedule}>Monday 29 January {value.username}
+                            <Link to="/doctor/meeting">
+                                <img src={check} alt="check" className={style.check} onClick={() => handleConfirm(value)} />
+                            </Link>
+                        </li>)
+                    })}
                 </ul>
             </div>
         </>
